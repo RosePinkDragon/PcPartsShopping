3c3aac25e95331e1972ff34c65f2d20859bed3be
RosePinkDragon <59683036+RosePinkDragon@users.noreply.github.com>
2020-09-20 20:03:56 +0530

    loving the new site

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        crimson: '#DC143C'
+
+      }
+
+      
+    },
+  },
+  variants: {},
+  plugins: [],
+}
